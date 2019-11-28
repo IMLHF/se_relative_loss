a51a940fe7241756deacee3279dafbc194202113
IMLHF <red_wind@foxmail.com>
2019-11-28 14:23:49 +0800

    add vctk&MUSAN datasets

---
+++ vctk_musan_datasets_8k/get_100_spk.sh
@@ -0,0 +1,101 @@
+cp ./wav/C001* ./aishell2_100spk_16k -r  # 25 north man
+cp ./wav/C002* ./aishell2_100spk_16k -r
+cp ./wav/C0030 ./aishell2_100spk_16k -r
+cp ./wav/C0031 ./aishell2_100spk_16k -r
+cp ./wav/C0032 ./aishell2_100spk_16k -r
+cp ./wav/C0033 ./aishell2_100spk_16k -r
+cp ./wav/C0034 ./aishell2_100spk_16k -r
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cp ./wav/D0022 ./aishell2_100spk_16k -r # 25 north woman
+cp ./wav/D0023 ./aishell2_100spk_16k -r
+cp ./wav/D0024 ./aishell2_100spk_16k -r
+cp ./wav/D0025 ./aishell2_100spk_16k -r
+cp ./wav/D0026 ./aishell2_100spk_16k -r  
+cp ./wav/D0027 ./aishell2_100spk_16k -r
+cp ./wav/D0028 ./aishell2_100spk_16k -r
+cp ./wav/D0029 ./aishell2_100spk_16k -r
+cp ./wav/D0032 ./aishell2_100spk_16k -r
+cp ./wav/D0033 ./aishell2_100spk_16k -r
+cp ./wav/D0034 ./aishell2_100spk_16k -r
+cp ./wav/D0035 ./aishell2_100spk_16k -r
+cp ./wav/D0036 ./aishell2_100spk_16k -r
+cp ./wav/D0037 ./aishell2_100spk_16k -r
+cp ./wav/D0038 ./aishell2_100spk_16k -r
+cp ./wav/D0039 ./aishell2_100spk_16k -r
+cp ./wav/D0040 ./aishell2_100spk_16k -r
+cp ./wav/D0041 ./aishell2_100spk_16k -r
+cp ./wav/D0042 ./aishell2_100spk_16k -r
+cp ./wav/D0400 ./aishell2_100spk_16k -r
+cp ./wav/D0401 ./aishell2_100spk_16k -r
+cp ./wav/D0409 ./aishell2_100spk_16k -r
+cp ./wav/D0410 ./aishell2_100spk_16k -r
+cp ./wav/D0413 ./aishell2_100spk_16k -r
+cp ./wav/D0414 ./aishell2_100spk_16k -r
+cp ./wav/D0415 ./aishell2_100spk_16k -r
+cp ./wav/C0249 ./aishell2_100spk_16k -r # 25 south man
+cp ./wav/C0272 ./aishell2_100spk_16k -r
+cp ./wav/C0596 ./aishell2_100spk_16k -r
+cp ./wav/C0597 ./aishell2_100spk_16k -r
+cp ./wav/C0598 ./aishell2_100spk_16k -r
+cp ./wav/C0599 ./aishell2_100spk_16k -r
+cp ./wav/C0600 ./aishell2_100spk_16k -r
+cp ./wav/D0329 ./aishell2_100spk_16k -r
+cp ./wav/C0293 ./aishell2_100spk_16k -r
+cp ./wav/C0369 ./aishell2_100spk_16k -r
+cp ./wav/C0412 ./aishell2_100spk_16k -r
+cp ./wav/C0536 ./aishell2_100spk_16k -r
+cp ./wav/C0792 ./aishell2_100spk_16k -r
+cp ./wav/D0150 ./aishell2_100spk_16k -r
+cp ./wav/D0066 ./aishell2_100spk_16k -r
+cp ./wav/D0076 ./aishell2_100spk_16k -r
+cp ./wav/D0215 ./aishell2_100spk_16k -r
+cp ./wav/D0218 ./aishell2_100spk_16k -r
+cp ./wav/D0299 ./aishell2_100spk_16k -r
+cp ./wav/D0310 ./aishell2_100spk_16k -r
+cp ./wav/D0115 ./aishell2_100spk_16k -r
+cp ./wav/D0240 ./aishell2_100spk_16k -r
+cp ./wav/D0346 ./aishell2_100spk_16k -r
+cp ./wav/D0352 ./aishell2_100spk_16k -r
+cp ./wav/D0356 ./aishell2_100spk_16k -r
+cp ./wav/D0296 ./aishell2_100spk_16k -r # 25 south woman
+cp ./wav/D0297 ./aishell2_100spk_16k -r
+cp ./wav/D0304 ./aishell2_100spk_16k -r
+cp ./wav/D0307 ./aishell2_100spk_16k -r
+cp ./wav/D0308 ./aishell2_100spk_16k -r
+cp ./wav/D0318 ./aishell2_100spk_16k -r
+cp ./wav/C0594 ./aishell2_100spk_16k -r
+cp ./wav/D0038 ./aishell2_100spk_16k -r
+cp ./wav/C0317 ./aishell2_100spk_16k -r
+cp ./wav/C0338 ./aishell2_100spk_16k -r
+cp ./wav/D0088 ./aishell2_100spk_16k -r
+cp ./wav/C0436 ./aishell2_100spk_16k -r
+cp ./wav/C0838 ./aishell2_100spk_16k -r
+cp ./wav/D0074 ./aishell2_100spk_16k -r
+cp ./wav/D0114 ./aishell2_100spk_16k -r
+cp ./wav/D0113 ./aishell2_100spk_16k -r
+cp ./wav/D0333 ./aishell2_100spk_16k -r
+cp ./wav/D0334 ./aishell2_100spk_16k -r
+cp ./wav/C0949 ./aishell2_100spk_16k -r
+cp ./wav/D0347 ./aishell2_100spk_16k -r
+cp ./wav/D0348 ./aishell2_100spk_16k -r
+cp ./wav/D0350 ./aishell2_100spk_16k -r
+cp ./wav/D0359 ./aishell2_100spk_16k -r
+cp ./wav/D1001 ./aishell2_100spk_16k -r
+cp ./wav/D1003 ./aishell2_100spk_16k -r
+
