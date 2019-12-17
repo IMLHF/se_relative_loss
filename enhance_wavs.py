2a4c96f405281af14da769fe20675ce4b784cf5e
IMLHF <red_wind@foxmail.com>
2019-12-17 10:34:15 +0800

    fix bug

--- enhance_wavs.py
+++ enhance_wavs.py
@@ -14,11 +14,20 @@ from nn_se.utils.assess.core import calc_pesq, calc_stoi, calc_sdr, calc_SegSNR
 smg = None
 
 noisy_and_ref_list = [
-  ('exp/paper_sample/0db_mix_ref/p265_015_nfree_0678.wav', 'exp/paper_sample/0db_mix_ref/p265_015.wav'),
-  ('exp/paper_sample/0db_mix_ref/p265_026_nfree_0571.wav', 'exp/paper_sample/0db_mix_ref/p265_026.wav'),
-  ('exp/paper_sample/0db_mix_ref/p265_038_nfree_0758.wav', 'exp/paper_sample/0db_mix_ref/p265_038.wav'),
-  ('exp/paper_sample/0db_mix_ref/p267_087_nfree_0663.wav', 'exp/paper_sample/0db_mix_ref/p267_087.wav'),
+  # ('exp/paper_sample/0db_mix_ref/p265_015_nfree_0678.wav', 'exp/paper_sample/0db_mix_ref/p265_015.wav'),
+  # ('exp/paper_sample/0db_mix_ref/p265_026_nfree_0571.wav', 'exp/paper_sample/0db_mix_ref/p265_026.wav'),
+  # ('exp/paper_sample/0db_mix_ref/p265_038_nfree_0758.wav', 'exp/paper_sample/0db_mix_ref/p265_038.wav'),
+  # ('exp/paper_sample/0db_mix_ref/p267_087_nfree_0663.wav', 'exp/paper_sample/0db_mix_ref/p267_087.wav'),
+  # ('exp/paper_sample/voxceleb1_sample/mixed/id10001_1zcIwhmdeo4_00001.wav', None),
+  # ('exp/paper_sample/voxceleb1_sample/mixed/id10001_1zcIwhmdeo4_00002.wav', None),
+  # ('exp/paper_sample/voxceleb1_sample/mixed/id10001_1zcIwhmdeo4_00003.wav', None),
+  ('exp/paper_sample/voxceleb1_sample/mixed/id10001_utrA-v8pPm4_00001.wav', None),
+  ('exp/paper_sample/voxceleb1_sample/mixed/id10006_3RybHF5mX78_00001.wav', None),
+  # ('exp/paper_sample/voxceleb1_sample/mixed/id10006_3RybHF5mX78_00002.wav', None),
+  ('exp/paper_sample/voxceleb1_sample/mixed/id10006_5tGaUGO_z50_00001.wav', None),
+  ('exp/paper_sample/voxceleb1_sample/mixed/id10006_zQROl4ZsMVA_00002.wav', None),
 ]
+save_dir = 'exp/paper_sample/voxceleb1_sample'
 
 def magnitude_spectrum_librosa_stft(signal, NFFT, overlap):
   signal = np.array(signal, dtype=np.float)
@@ -35,7 +44,6 @@ def enhance_and_calcMetrics(noisy_and_ref):
   noisy_wav, sr = audio.read_audio(noisy_wav_dir)
   ref_wav, sr = audio.read_audio(ref_wav_dir) if ref_wav_dir else (None, sr)
   config_name = FLAGS.PARAM().config_name()
-  save_dir = 'exp/paper_sample'
   noisy_stem = Path(noisy_wav_dir).stem
   global smg
   if smg is None:
@@ -49,26 +57,27 @@ def enhance_and_calcMetrics(noisy_and_ref):
   name_prefix = "%s_%s" % (config_name, noisy_stem)
 
   # calc metrics [pesq_noisy->pesq_enhanced | pesqi, stoi, sdr, ssnr]
-  pesq_noisy = calc_pesq(ref_wav, noisy_wav, sr)
-  pesq_enhanced = calc_pesq(ref_wav, enhanced_wav, sr)
-  pesqi = pesq_enhanced - pesq_noisy
-  stoi_noisy = calc_stoi(ref_wav, noisy_wav, sr)
-  stoi_enhanced = calc_stoi(ref_wav, enhanced_wav, sr)
-  stoii = stoi_enhanced - stoi_noisy
-  sdr_noisy = calc_sdr(ref_wav, noisy_wav, sr)
-  sdr_enhanced = calc_sdr(ref_wav, enhanced_wav, sr)
-  sdri = sdr_enhanced - sdr_noisy
-  ssnr_noisy = calc_SegSNR(ref_wav/np.max(ref_wav), noisy_wav/np.max(noisy_wav), 256, 256)
-  ssnr_enhanced = calc_SegSNR(ref_wav/np.max(ref_wav), enhanced_wav/np.max(enhanced_wav), 256, 256)
-  ssnri = ssnr_enhanced - ssnr_noisy
+  if ref_wav is not None:
+    pesq_noisy = calc_pesq(ref_wav, noisy_wav, sr)
+    pesq_enhanced = calc_pesq(ref_wav, enhanced_wav, sr)
+    pesqi = pesq_enhanced - pesq_noisy
+    stoi_noisy = calc_stoi(ref_wav, noisy_wav, sr)
+    stoi_enhanced = calc_stoi(ref_wav, enhanced_wav, sr)
+    stoii = stoi_enhanced - stoi_noisy
+    sdr_noisy = calc_sdr(ref_wav, noisy_wav, sr)
+    sdr_enhanced = calc_sdr(ref_wav, enhanced_wav, sr)
+    sdri = sdr_enhanced - sdr_noisy
+    ssnr_noisy = calc_SegSNR(ref_wav/np.max(ref_wav), noisy_wav/np.max(noisy_wav), 256, 256)
+    ssnr_enhanced = calc_SegSNR(ref_wav/np.max(ref_wav), enhanced_wav/np.max(enhanced_wav), 256, 256)
+    ssnri = ssnr_enhanced - ssnr_noisy
 
-  metrics_eval_ans_f = "%s_metrics_eval_ans.log" % config_name
-  with open(os.path.join(save_dir, metrics_eval_ans_f), 'a') as f:
-    f.write(name_prefix+":\n")
-    f.write("    pesq: %.3f->%.3f, pesqi: %.3f.\n" % (pesq_noisy, pesq_enhanced, pesqi))
-    f.write("    stoi: %.3f->%.3f, stoii: %.3f.\n" % (stoi_noisy, stoi_enhanced, stoii))
-    f.write("    sdr : %.3f->%.3f, sdri : %.3f.\n" % (sdr_noisy, sdr_enhanced, sdri))
-    f.write("    ssnr: %.3f->%.3f, ssnri: %.3f.\n" % (ssnr_noisy, ssnr_enhanced, ssnri))
+    metrics_eval_ans_f = "%s_metrics_eval_ans.log" % config_name
+    with open(os.path.join(save_dir, metrics_eval_ans_f), 'a') as f:
+      f.write(name_prefix+":\n")
+      f.write("    pesq: %.3f->%.3f, pesqi: %.3f.\n" % (pesq_noisy, pesq_enhanced, pesqi))
+      f.write("    stoi: %.3f->%.3f, stoii: %.3f.\n" % (stoi_noisy, stoi_enhanced, stoii))
+      f.write("    sdr : %.3f->%.3f, sdri : %.3f.\n" % (sdr_noisy, sdr_enhanced, sdri))
+      f.write("    ssnr: %.3f->%.3f, ssnri: %.3f.\n" % (ssnr_noisy, ssnr_enhanced, ssnri))
 
   # get x_ticks
   n_frame = np.shape(mask)[0]
